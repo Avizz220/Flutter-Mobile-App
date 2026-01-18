a06053c77d49fc0c498ff21cf65e6386f28fcf89
Avishka2002 <ahirushan629@gmail.com>
2026-01-18 18:43:06 +0530

    Edited the change themse feature in the settings page

--- mytodoapp_frontend/lib/features/settings/ui/theme_selector.dart
+++ mytodoapp_frontend/lib/features/settings/ui/theme_selector.dart
@@ -142,7 +142,7 @@ class _ThemeSelectorScreenState extends State<ThemeSelectorScreen> {
                         crossAxisCount: 2,
                         crossAxisSpacing: 15,
                         mainAxisSpacing: 15,
-                        childAspectRatio: 1.5,
+                        childAspectRatio: 1.1,
                       ),
                       itemCount: ThemeService.themeColors.length,
                       itemBuilder: (context, index) {
@@ -156,7 +156,10 @@ class _ThemeSelectorScreenState extends State<ThemeSelectorScreen> {
                           onTap: () => _saveTheme(colorName),
                           child: AnimatedContainer(
                             duration: Duration(milliseconds: 200),
-                            padding: EdgeInsets.all(20),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 20,
+                            ),
                             decoration: BoxDecoration(
                               color: isDark ? Color(0xFF1E1E1E) : Colors.white,
                               borderRadius: BorderRadius.circular(15),
@@ -182,6 +185,7 @@ class _ThemeSelectorScreenState extends State<ThemeSelectorScreen> {
                             ),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
                               children: [
                                 // Color Circle
                                 Container(
@@ -211,9 +215,12 @@ class _ThemeSelectorScreenState extends State<ThemeSelectorScreen> {
                                 // Color Name
                                 Text(
                                   colorName,
+                                  textAlign: TextAlign.center,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
                                   style: TextStyle(
                                     fontFamily: 'Poppins',
-                                    fontSize: 16,
+                                    fontSize: 15,
                                     fontWeight:
                                         isSelected
                                             ? FontWeight.w600
